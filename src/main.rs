7d4fbf09adc58eb359905dda4e2171436d5d51e7
KarasawaTakahiro <karasawa@basicinc.jp>
2022-11-15 03:04:57 +0900

    feat: cli

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,32 @@
 use async_graphql::{EmptyMutation, EmptySubscription, Schema};
+use clap::{Parser, Subcommand};
 
 use ganttchart::graphql::Query;
 use ganttchart::server;
 
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    Server,
+    Dump,
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let schema = Schema::build(Query::default(), EmptyMutation, EmptySubscription).finish();
+    let cli = Cli::parse();
 
-    println!("{}", &schema.sdl());
+    let schema = || Schema::build(Query::default(), EmptyMutation, EmptySubscription).finish();
 
-    server::server(schema.clone()).await?.await
+    match cli.command {
+        Command::Server => server::server(schema().clone()).await?.await,
+        Command::Dump => {
+            println!("{}", &schema().sdl());
+            Ok(())
+        }
+    }
 }
