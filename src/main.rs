5f0ac0255c9acbe56bc9513a34008eb97a5a77dd
KarasawaTakahiro <karasawa@basicinc.jp>
2022-11-14 18:44:48 +0900

    feat: CORS を無視する
    開発中はとりあえず無視して

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use actix_cors::Cors;
 use actix_web::{guard, web, web::Data, App, HttpResponse, HttpServer, Result};
 use async_graphql::{http::GraphiQLSource, EmptyMutation, EmptySubscription, Schema};
 use async_graphql_actix_web::{GraphQLRequest, GraphQLResponse};
@@ -25,7 +26,13 @@ async fn main() -> std::io::Result<()> {
     println!("{}", &schema.sdl());
 
     HttpServer::new(move || {
+        let cors = Cors::default()
+            .allow_any_origin()
+            .allow_any_method()
+            .allow_any_header();
+
         App::new()
+            .wrap(cors)
             .app_data(Data::new(schema.clone()))
             .service(web::resource("/graphql").guard(guard::Post()).to(index))
             .service(web::resource("/").guard(guard::Get()).to(graphiql))
