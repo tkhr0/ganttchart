eb19d2b459eb4fc00e55698e54210205d91a422d
KarasawaTakahiro <karasawa@basicinc.jp>
2022-11-16 19:27:49 +0900

    feat: サンプルクエリを作成
    ネストさせたクエリ

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,14 @@
 import ReactDOM from "react-dom/client";
 
 import { Tasks } from "./features/tasks";
+import { Organizations } from "./features/organizations";
 import { Provider } from "urql";
 import { client } from "./graphql/client";
 
 const App = () => (
   <Provider value={client}>
     <Tasks />
+    <Organizations />
   </Provider>
 );
 
---
+++ src/features/organizations/components/Organizations.tsx
@@ -0,0 +1,54 @@
+import { useQuery } from "urql";
+
+import { graphql } from "../../../gql";
+import type { Organization, Project, Team } from "../../../gql/graphql";
+
+const organizationQuery = graphql(`
+  query organizations {
+    organizations {
+      name
+
+      projects {
+        name
+
+        teams {
+          name
+        }
+      }
+    }
+  }
+`);
+
+export const Organizations = (): JSX.Element => {
+  const [{ data, fetching }] = useQuery({ query: organizationQuery });
+
+  if (fetching) {
+    return <></>;
+  }
+
+  return (
+    <ul>
+      {data?.organizations.map((organization: Organization, i: number) => {
+        return (
+          <li key={i}>
+            <p>{organization.name}</p>
+            <ul>
+              {organization.projects.map((project: Project, j: number) => {
+                return (
+                  <li key={j}>
+                    <p>{project.name}</p>
+                    <ul>
+                      {project.teams.map((team: Team, k) => {
+                        return <li key={k}>{team.name}</li>;
+                      })}
+                    </ul>
+                  </li>
+                );
+              })}
+            </ul>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
